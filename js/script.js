67afba93430bc465c0e295ba1bfb572cee491178
admin <admin@admins-MacBook-Pro.local>
2022-08-23 11:32:27 -0400

    scoring logic and display added

--- js/script.js
+++ js/script.js
@@ -31,12 +31,17 @@
 // 	console.log(response);
 // });
 
+//GLOBAL VARIABLES
+
+let score = 0;
+
 //DOM ELEMENTS
+
 const $word = $('#word');
 const $definition = $('#definition');
 const $yes = $('#yes');
 const $no = $('#no');
-
+const $score = $('#score');
 
 //FUNCTIONS
 
@@ -45,41 +50,52 @@ const $no = $('#no');
 //input: takes no input
 //output: true if player's guess is successful and false if it is unsuccessful
 ////////////////////////////////////////////////////////////////////////////////////
-function render(){
+function render() {
 
-//decide if word will be real or fake (50/50)
-let word = {word: "", definition: ""};
-const isReal = Math.round(Math.random());
+    $score.text(score, score); //display score of 0
 
-if (isReal){ //real word chosen
-    word = findRealWord();
-}
-else { //randomly choose between 2 procedures for fake word (50/50):
-    const fakeA = Math.round(Math.random());
-    if (fakeA){
-        word = findFakeWordA();
+    //decide if word will be real or fake (50/50)
+    const isReal = Math.round(Math.random());
+
+    if (isReal) { //real word chosen
+        word = findRealWord();
     }
-    else {
-        word = findFakeWordB();
+    else { //randomly choose between 2 procedures for fake word (50/50):
+        const fakeA = Math.round(Math.random());
+        if (fakeA) {
+            word = findFakeWordA();
+        }
+        else {
+            word = findFakeWordB();
+        }
     }
-}
 
-console.log(word);
+    //display rendered word and definition
+    $word.text(word.word);
+    $definition.text(word.definition);
 
-//display rendered word and definition
-$word.text(word.word);
-$definition.text(word.definition);
+    //listen for user choice
+    $yes.on('click', function () {        
+        if (isReal){
+            scoreAdd(10);
+        }
+    })
 
-//listen for user choice
+    $no.on('click', function () {
+        if (!isReal){
+            scoreAdd(10);
+        }
+    })
 
-/////enclose in a score function
-//if successful +10 points!
-//if unsuccessful +0 points :(
-/////enclose in a score function
 
-//if number of questions is still > total, back to beginning
-//else, display final score 
-//MVP game is finished
+    /////enclose in a score function
+    //if successful +10 points!
+    //if unsuccessful +0 points :(
+    /////enclose in a score function
+
+    //if number of questions is still > total, back to beginning
+    //else, display final score 
+    //MVP game is finished
 
 }
 
@@ -88,17 +104,15 @@ $definition.text(word.definition);
 //input: takes no input
 //output: returns a object with one string key-value pair - {word: realWord, definition: realDefinition}
 ////////////////////////////////////////////////////////////////////////////////////
-function findRealWord(){
+function findRealWord() {
+    return { word: 'a-real-word', definition: 'a real word definition here' };
 
-    console.log("Finding a real word");
-    return {word: 'a real word', definition: 'a real word definition here'};
+    //API call for a random word
 
-//API call for a random word
+    //API call this word to check if word is uncommon 
+    //if common, back to beginning of function
 
-//API call this word to check if word is uncommon 
-//if common, back to beginning of function
-
-//if uncommon, return a string object in the format {word: definition}
+    //if uncommon, return a string object in the format {word: definition}
 
 }
 
@@ -107,14 +121,13 @@ function findRealWord(){
 //input: takes no input
 //output: returns a object with one string key-value pair - {word: fakeWord, definition: fakeDefinition}
 ////////////////////////////////////////////////////////////////////////////////////
-function findFakeWordA(){
-    console.log("Finding fake words A");
-    return {word: 'fake-concatenated word', definition: 'and some BS nonsense about what it means'}
+function findFakeWordA() {
+    return { word: 'fake-concatenated-word', definition: 'and some BS nonsense about what it means' }
 
-//API call for two random words that fit criteria (fine tuning and play testing needed)
-//trim and concantenate them in a way that is feasible (maybe look up "how to make fake words")
-//randomly select one word's definition to be the final definition (50/50)
-//return a string object in the format {word: definition}
+    //API call for two random words that fit criteria (fine tuning and play testing needed)
+    //trim and concantenate them in a way that is feasible (maybe look up "how to make fake words")
+    //randomly select one word's definition to be the final definition (50/50)
+    //return a string object in the format {word: definition}
 
 }
 
@@ -123,21 +136,29 @@ function findFakeWordA(){
 //input: takes no input
 //output: returns a object with one string key-value pair - {word: fakeWord, definition: fakeDefinition}
 ////////////////////////////////////////////////////////////////////////////////////
-function findFakeWordB(){
-    console.log("Finding fake words B");
-    return {word: 'fake-letter-swap-word', definition: 'and some BS nonsense about what it means'}
+function findFakeWordB() {
+    return { word: 'fake-letter-swap-word', definition: 'and some BS nonsense about what it means' }
 
-//API call for one random word that fit criteria (fine tuning and play testing needed)
-//swap vowels and consonants in a convincing way (requires some planning)
-//find another random words definition, attach this to the the word object
-//return a string object in the format {word: definition}
+    //API call for one random word that fit criteria (fine tuning and play testing needed)
+    //swap vowels and consonants in a convincing way (requires some planning)
+    //find another random words definition, attach this to the the word object
+    //return a string object in the format {word: definition}
 
 }
 
+////////////////////////////////////////////////////////////////////////////////////
+//purpose: add points to score
+//input: takes number of points to add
+//output: none
+////////////////////////////////////////////////////////////////////////////////////
+function scoreAdd(num) {
+    score += num;
+    $score.text(score);
+    return num;
+}
 
 
-
-function main(){
+function main() {
 
     render();
 
