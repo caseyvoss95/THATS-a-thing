0abffc4473f4074565fab1bc94788140f3095682
admin <admin@admins-MacBook-Pro.local>
2022-08-24 13:55:16 -0400

    real word and definition appear, MVP reached

--- js/script.js
+++ js/script.js
@@ -31,10 +31,19 @@
 // 	console.log(response);
 // });
 
+
+
+
+
+
+
 //GLOBAL VARIABLES
 
 let score = 0;
 let questionNum = 5;
+let currentWord;
+let isReal;
+
 
 //DOM ELEMENTS
 
@@ -49,17 +58,18 @@ const $questionsNum = $('#questionsNum');
 
 ////////////////////////////////////////////////////////////////////////////////////
 //purpose: renders a question in the game
-//input: takes no input
+//input: none
 //output: true if word is real, false if word is fake
 ////////////////////////////////////////////////////////////////////////////////////
 function render() {
 
     //decide if word will be real or fake (50/50)
     //const isReal = Math.round(Math.random());
-    const isReal = true; //DEBUG ONLY
+    isReal = true; //DEBUG ONLY
 
     if (isReal) { //real word chosen
-        word = findRealWord();
+        findRealWord();
+        console.log(currentWord);
     }
     else { //randomly choose between 2 procedures for fake word (50/50):
         const fake = Math.round(Math.random());
@@ -79,59 +89,11 @@ function render() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
-//purpose: collect a random genuine word and definition
-//input: takes no input
-//output: returns an object with one string key-value pair - {word: realWord, definition: realDefinition}
-////////////////////////////////////////////////////////////////////////////////////
-function findRealWord() {
-    
-    //API call for a random word
-    let wordObject = getRandomWord();
-    console.log(wordObject);
-
-    //API call this word to check if word is uncommon 
-    //if common, back to beginning of function
-
-    //if uncommon, return a string object in the format {word: definition}
-
-}
-
-////////////////////////////////////////////////////////////////////////////////////
-//purpose: fabricate a convincing fake word from two real words
-//input: takes no input
-//output: returns an object with one string key-value pair - {word: fakeWord, definition: fakeDefinition}
-////////////////////////////////////////////////////////////////////////////////////
-function findFakeWordA() {
-    return { word: 'fake-concatenated-word', definition: 'and some BS nonsense about what it means' }
-
-    //API call for two random words that fit criteria (fine tuning and play testing needed)
-    //trim and concantenate them in a way that is feasible (maybe look up "how to make fake words")
-    //randomly select one word's definition to be the final definition (50/50)
-    //return a string object in the format {word: definition}
-
-}
-
-////////////////////////////////////////////////////////////////////////////////////
-//purpose: fabricate a convincing fake word by swapping vowels and consonants
-//input: takes no input
-//output: returns a object with one string key-value pair - {word: fakeWord, definition: fakeDefinition}
-////////////////////////////////////////////////////////////////////////////////////
-function findFakeWordB() {
-    return { word: 'fake-letter-swap-word', definition: 'and some BS nonsense about what it means' }
-
-    //API call for one random word that fits criteria (fine tuning and play testing needed)
-    //swap vowels and consonants in a convincing way (requires some planning)
-    //find another random words definition, attach this to the the word object
-    //return a string object in the format {word: definition}
-
-}
-
-////////////////////////////////////////////////////////////////////////////////////
-//purpose: retrieve a random word and definition from Words API
+//purpose: retrieve a random word from Words API
 //input: none
-//output: returns an object in this fashion - {word: wordActual, definition: definitionActual}
+//output: a string representing a random word
 ////////////////////////////////////////////////////////////////////////////////////
-function getRandomWord() {
+const getRandomWord = function () {
 
     //random word API call
     const settings = {
@@ -145,17 +107,147 @@ function getRandomWord() {
         }
     };
 
-    $.ajax(settings).done(function (wordActual) {
-        console.log(wordActual);
-        return { word: wordActual.word, definition: wordActual.definition }
-    });
+        $.ajax(settings).then((wordActual) => {
+            currentWord = wordActual;
+            $word.text(currentWord.word);
+            
+            
+            console.log(wordActual.word);
+
+            const settingsA = {
+                "async": true,
+                "crossDomain": true,
+                "url": `https://wordsapiv1.p.rapidapi.com/words/${wordActual.word}/definitions`,
+                "method": "GET",
+                "headers": {
+                    "X-RapidAPI-Key": "dc2e0e8bddmshc3267816db39455p18c965jsn6c05ba4f9f24",
+                    "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
+                }
+            };
+            
+            $.ajax(settingsA).then(function (wordDefinition) {
+                console.log(wordDefinition.definitions[0].definition);
+                $definition.text(wordDefinition.definitions[0].definition);
+
+
+            });
+
+
+
+
+
+
+           
+        })
+    console.log(currentWord);
+
+    //})
+    return currentWord;
+}
+
+//getRandomWord();
+
+////////////////////////////////////////////////////////////////////////////////////
+//purpose: collect a random genuine word and definition
+//input: none
+//output: an object in the format - {word: realWord, definition: realDefinition}
+////////////////////////////////////////////////////////////////////////////////////
+const findRealWord = function () {
+
+    //API call for a random word
+    //const wordActual2 = getRandomWord();
+
+    console.log(currentWord);
+
+    let garbage = getRandomWord();
+    return garbage;
+    //console.log(garbage);
+
+
+
+
+    //API call to check frequency of use for word
+    //let wordCheck = wordObject.word;
+    //console.log(typeof checkFrequency("the"));
+
+    //if common, back to beginning of function
+    //if uncommon, API call to retrieve definition of word
+
+    //return a string object in the format {word: definition}
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+//purpose: fabricate a convincing fake word from two real words
+//input: none
+//output: returns an object with one string key-value pair - {word: fakeWord, definition: fakeDefinition}
+////////////////////////////////////////////////////////////////////////////////////
+function findFakeWordA() {
+    return { word: 'fake-concatenated-word', definition: 'and some BS nonsense about what it means' }
+
+    //API calls for two random words that fit criteria (fine tuning and play testing needed)
+    //trim and concantenate them in a way that is feasible (maybe look up "how to make fake words")
+    //randomly select one word's definition to be the final definition (50/50)
+    //return a string object in the format {word: fakeWord, definition: fakeDefinition}
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+//purpose: fabricate a convincing fake word by swapping vowels and consonants
+//input: none
+//output: an object with one string key-value pair - {word: fakeWord, definition: fakeDefinition}
+////////////////////////////////////////////////////////////////////////////////////
+function findFakeWordB() {
+    return { word: 'fake-letter-swap-word', definition: 'and some BS nonsense about what it means' }
+
+    //API call for one random word that fits criteria (fine tuning and play testing needed)
+    //swap vowels and consonants in a convincing way (requires some planning)
+    //API call for  another random words definition, attach this to the the word object
+    //return a string object in the format {word: fakeWord, definition: fakeDefinition}
 
 }
 
 
+// console.log(wordA);
+// wordA = wordActual.word;
+// console.log(wordA);
+// console.log(wordActual.word);
+// def = wordActual.def;
+// NOTES: the JSON object is completely accessible in this scope. I need to figure out how to save the data I found here and use it outside of this scope. look up then() method.
+
+
 ////////////////////////////////////////////////////////////////////////////////////
-//purpose: add points to score
-//input: takes number of points to add
+//purpose: check the frequency of a use of a given word via Words API
+//input: string of word being checked
+//output: number score from 1 - 7
+////////////////////////////////////////////////////////////////////////////////////
+function checkFrequency(word) {
+
+    //frequency API call
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://wordsapiv1.p.rapidapi.com/words/${word}/frequency`,
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "dc2e0e8bddmshc3267816db39455p18c965jsn6c05ba4f9f24",
+            "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
+        }
+    };
+    return 'foo';
+    $.ajax(settings).done(function (wordActual) {
+        console.log(wordActual.frequency.zipf);
+
+        return 'foo';
+        //return typeof wordActual.frequency.zipf;
+    });
+
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+//purpose: add points to score and updates score display
+//input: number of points to add
 //output: none
 ////////////////////////////////////////////////////////////////////////////////////
 function scoreAdd(num) {
@@ -164,7 +256,7 @@ function scoreAdd(num) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
-//purpose: subtracts and displays one question from the count
+//purpose: subtracts question from the count and updates question quantity display
 //input: none
 //output: none
 ////////////////////////////////////////////////////////////////////////////////////
@@ -199,12 +291,14 @@ function gameOver() {
 
 function main() {
 
-    $score.text(score); //display score of 0
+    //set counters
+    $score.text(score);
     $questionsNum.text(questionNum);
 
-    let isReal = render();
+    //render question
+    render();
 
-    //listen for user choices
+    //listen for user choices TODO: DRY it up
     $yes.on('click', function () {
 
         if (isReal) {
